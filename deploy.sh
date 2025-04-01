87c611e4b043a06187cb81740c40e50a48a19e01
Gregory Hall <76702053+DataGuys@users.noreply.github.com>
2025-04-01 16:58:21 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,21 +13,21 @@ selectedSub=$(az account list --query "[$((subNum-1))].id" -o tsv)
 az account set --subscription "$selectedSub"
 echo "Using subscription: $selectedSub"
 
-# Prompt for the resource group name
+# Prompt for the Resource Group name
 read -p "Enter the Resource Group name: " resourceGroup
 
 # Define ACR name (must be all lowercase)
 acrName="myregistry"
 
-# Create the Azure Container Registry
-az acr create --resource-group "$resourceGroup" --name "$acrName" --sku Basic
+# Create the Azure Container Registry using a supported API version (e.g., 2024-03-01)
+az acr create --resource-group "$resourceGroup" --name "$acrName" --sku Basic --api-version 2024-03-01
 
 # Build the container image from GitHub and push it to ACR
 az acr build --registry "$acrName" --image vigilant-web:latest https://github.com/DataGuys/vigilant-web.git
 
-# Retrieve ACR credentials using an explicit API version
-username=$(az acr credential show --name "$acrName" --query "username" -o tsv --api-version 2023-07-01)
-password=$(az acr credential show --name "$acrName" --query "passwords[0].value" -o tsv --api-version 2023-07-01)
+# Retrieve ACR credentials (remove --api-version since it's unsupported)
+username=$(az acr credential show --name "$acrName" --query "username" -o tsv)
+password=$(az acr credential show --name "$acrName" --query "passwords[0].value" -o tsv)
 
 # Deploy the container image to an Azure Container Instance with registry credentials
 az container create \
